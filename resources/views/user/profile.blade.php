d470828c5e67e4206e884a50d0ae05b85fd51ac6
Dario <dario@mail.com>
2023-03-26 00:38:35 +0100

    Enable privacy

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -9,24 +9,33 @@
             <img class="w-32 mx-auto rounded-full -mt-20 border-8 border-white"
                  src="{{$user->getProfilePhotoUrl()}}" alt="">
             <div class="text-center mt-2 text-3xl font-medium">{{$user->getName()}}</div>
-            <div class=" text-center mt-2 font-light text-sm">@devpenzil
-            </div>
-            <div class="text-center font-normal text-lg">Kerala</div>
-            <div class="px-6 text-center mt-2 font-light text-sm">
-                <p>
-                    Front end Developer, avid reader. Love to take a long walk, swim
-                </p>
-            </div>
+            <?php
+            /*
+                       <div class=" text-center mt-2 font-light text-sm">@devpenzil
+                       </div>
+                       <div class="text-center font-normal text-lg">Kerala</div>
+                       <div class="px-6 text-center mt-2 font-light text-sm">
+                           <p>
+                               Front end Developer, avid reader. Love to take a long walk, swim
+                           </p>
+                       </div>
+            */ ?>
             <hr class="mt-8">
             <div class="flex p-4">
                 <div class="w-1/2 text-center">
-                    <span class="font-bold">1.8 k</span> Followers
+                    {{__('Rating')}} <span class="font-bold">{{$user->profile->getRating()}}</span>
                 </div>
                 <div class="w-0 border border-gray-300">
 
                 </div>
                 <div class="w-1/2 text-center">
-                    <span class="font-bold">2.0 k</span> Following
+                    {{__('Phone')}} <span class="font-bold">
+                        @if ($user->getPhoneNumber())
+                            <a class="underline" href="tel:{{$user->getPhoneNumber()}}">{{$user->getPhoneNumber()}}</a>
+                        @else
+                            {{__('No phone')}}
+                        @endif
+                    </span>
                 </div>
             </div>
         </div>
