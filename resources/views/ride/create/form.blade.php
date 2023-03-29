d57881f6cbdc72b7d32dbe3c1ca3e83a44d000e5
Dario <dario@nomail.com>
2023-03-29 21:37:23 +0200

    Fix datetime picker bug on mobile

--- resources/views/ride/create/form.blade.php
+++ resources/views/ride/create/form.blade.php
@@ -46,7 +46,10 @@
                         <div class="mb-6">
                             <label for="time"
                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{__('Minimum departure time')}}</label>
-                            <input type="text" id="time" name="time"
+                            <input type="text"
+                                   id="time"
+                                   name="time"
+                                   readonly
                                    value="{{old('time')}}"
                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                    required>
