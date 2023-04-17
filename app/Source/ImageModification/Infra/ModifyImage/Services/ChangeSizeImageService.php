25a6a5b03cdca806fff7a1e5962435a9f7eef9b9
Dario <dario@nomail.com>
2023-04-17 13:16:01 +0200

    Fix image orientation

--- app/Source/ImageModification/Infra/ModifyImage/Services/ChangeSizeImageService.php
+++ app/Source/ImageModification/Infra/ModifyImage/Services/ChangeSizeImageService.php
@@ -17,7 +17,6 @@ class ChangeSizeImageService
                 $constraint->aspectRatio();
                 $constraint->upsize();
             }
-        )
-            ->orientate();
+        );
     }
 }
