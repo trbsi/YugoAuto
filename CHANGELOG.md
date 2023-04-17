beab1c2b502b3d542311e6bb3f50448607d6bdbd
Dario <dario@nomail.com>
2023-04-17 11:22:30 +0200

    1.16.1 Change image picker to be regular input

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.16.1] - 2023-04-17
+
+### Changed
+
+- Changed image picker to be regular input(file) so image picker can work on Android via webview
+    - Fixed in mobile app in version 1.1.0
+
 ## [1.16.0] - 2023-04-16
 
 ### Added
--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -12,18 +12,6 @@
         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
             <div x-data="{photoName: null, photoPreview: null}" class="col-span-6 sm:col-span-4">
                 <!-- Profile Photo File Input -->
-                <input type="file" class="hidden"
-                       wire:model="photo"
-                       x-ref="photo"
-                       x-on:change="
-                                    photoName = $refs.photo.files[0].name;
-                                    const reader = new FileReader();
-                                    reader.onload = (e) => {
-                                        photoPreview = e.target.result;
-                                    };
-                                    reader.readAsDataURL($refs.photo.files[0]);
-                            "/>
-
                 <x-label for="photo" value="{{ __('Photo') }}"/>
 
                 <!-- Current Profile Photo -->
@@ -39,9 +27,24 @@
                     </span>
                 </div>
 
-                <x-secondary-button class="mt-2 mr-2" type="button" x-on:click.prevent="$refs.photo.click()">
+                <label for="file"
+                       class="inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150 mt-2 mr-2">
                     {{ __('Select A New Photo') }}
-                </x-secondary-button>
+                </label>
+                <input type="file"
+                       accept="image/*"
+                       class="hidden"
+                       id="file"
+                       wire:model="photo"
+                       x-ref="photo"
+                       x-on:change="
+                                    photoName = $refs.photo.files[0].name;
+                                    const reader = new FileReader();
+                                    reader.onload = (e) => {
+                                        photoPreview = e.target.result;
+                                    };
+                                    reader.readAsDataURL($refs.photo.files[0]);
+                            "/>
                 <div class="text-sm italic mt-1">
                     <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5" fill="currentColor"
                          viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -109,6 +112,10 @@
     </x-slot>
 
     <x-slot name="actions">
+        <x-label class="mr-1" wire:loading.block>
+            {{ __('Wait until it loads and then save') }}
+        </x-label>
+
         <x-action-message class="mr-3" on="saved">
             {{ __('Saved.') }}
         </x-action-message>
